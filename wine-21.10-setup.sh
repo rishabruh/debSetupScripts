bb83186076e993e939134f35eb46067e6f06e697
rishabh <91521594+rishabruh@users.noreply.github.com>
2021-12-02 10:03:37 +0530

    added LTS version script and regular script

---
+++ wine-21.10-setup.sh
@@ -0,0 +1,16 @@
+ echo Enabling 32-bit architecture...
+sudo dpkg --add-architecture i386
+
+echo Downloading and adding repo key...
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+
+echo Adding repo...
+#adding repo for ubuntu 21.10
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ impish main' 
+
+echo Updating packages and installing stable wine...
+sudo apt install --install-recommends winehq-stable
+
+echo Wine installed.
+
